690e11334572e0815b37d684b92d219260c528c7
wmwmss <xu.han.ethan@gmail.com>
2024-11-13 16:25:36 -0500

    Init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+ <Navbar />
+ <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,55 @@
+<script setup>
+// import jobData from '@/jobs.json'
+import { reactive, defineProps, onMounted } from 'vue'
+import JobListing from './JobListing.vue';
+import { RouterLink } from 'vue-router'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import axios from 'axios'
+
+// const jobs = ref([])
+// console.log(jobs.value)
+
+const state = reactive({
+    jobs: [],
+    isLoading: true
+})
+
+defineProps({
+    limit: Number,
+    showButton: {type: Boolean, default: false}
+})
+
+onMounted(async ()=> {
+    try{
+        const response = await axios.get('/api/jobs')
+        state.jobs = response.data
+    }catch(error){
+        console.error('Error fetching jobs', error)
+    }finally{
+        state.isLoading = false
+    }
+})
+</script>
+
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6">Browse Jobs</h2>
+            <!-- Show loading spinner while isLoading is true-->
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader></PulseLoader>
+            </div>
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" /> 
+            </div>
+        </div>
+    </section>
+
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <RouterLink
+        to="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</RouterLink
+      >
+    </section>
+</template>
